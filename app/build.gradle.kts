c14cfb18addf96a99b26a8da9c92bbe3d432d14f
hjimin027 <hjimin027@gmail.com>
2024-11-11 22:05:15 +0900

    gradle binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 android {
     namespace = "com.example.mobilesoftware_proj"
     compileSdk = 34
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.mobilesoftware_proj"
