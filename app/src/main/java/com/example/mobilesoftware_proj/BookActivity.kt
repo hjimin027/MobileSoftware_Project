983667ffe3d86e10d8f8632ea6df4bcd58b281f2
nunuhelios <nunuhelios@sookmyung.ac.kr>
2024-11-22 17:10:46 +0900

    add dateRangePicker

--- app/src/main/java/com/example/mobilesoftware_proj/BookActivity.kt
+++ app/src/main/java/com/example/mobilesoftware_proj/BookActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mobilesoftware_proj
 
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mobilesoftware_proj.databinding.ActivityBookBinding
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+import androidx.core.util.Pair
 
 class BookActivity : AppCompatActivity() {
     val binding by lazy { ActivityBookBinding.inflate(layoutInflater) }
@@ -14,7 +18,33 @@ class BookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-    }
 
+        binding.calendar.setOnClickListener{
+            val dateRangePicker =
+                MaterialDatePicker.Builder.dateRangePicker()
+                    .setTitleText("검색 기간을 골라주세요")
+//                    .setSelection(
+//                        Pair(
+//                            MaterialDatePicker.thisMonthInUtcMilliseconds(),
+//                            MaterialDatePicker.todayInUtcMilliseconds()
+//                        )
+//                    )
+                    .build()
+
+            dateRangePicker.show(supportFragmentManager, "date_picker")
+            dateRangePicker.addOnPositiveButtonClickListener(object :
+                MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>> {
+                    override fun onPositiveButtonClick(selection: Pair<Long, Long>?) {
+                        val calendar = Calendar.getInstance()
+                        calendar.timeInMillis = selection?.first ?: 0
+                        binding.startDate.text = SimpleDateFormat("yyyy.MM.dd").format(calendar.time).toString()
+
+                        calendar.timeInMillis = selection?.second ?: 0
+                         binding.endDate.text = SimpleDateFormat("yyyy.MM.dd").format(calendar.time).toString()
+                    }
+                })
+        }
+
+    }
 
 }
