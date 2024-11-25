be3e9eccb8d39e3e8826b4b7fc26f574b22cd120
nunuhelios <nunuhelios@sookmyung.ac.kr>
2024-11-26 00:03:54 +0900

    change info display format

--- app/src/main/java/com/example/mobilesoftware_proj/InfoActivity.kt
+++ app/src/main/java/com/example/mobilesoftware_proj/InfoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobilesoftware_proj
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.mobilesoftware_proj.databinding.ActivityInfoBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.util.Locale
 
 class InfoActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -23,6 +25,13 @@ class InfoActivity : AppCompatActivity() {
         binding.infoEmailText.text = auth.currentUser?.email
 
         // 로그인한 날짜
-        binding.infoDateText.text = auth.currentUser?.metadata?.creationTimestamp.toString()
+        //binding.infoDateText.text = auth.currentUser?.metadata?.creationTimestamp.toString()
+        val creationTime = auth.currentUser?.metadata?.creationTimestamp
+        if (creationTime != null) {
+            val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
+            binding.infoDateText.text = dateFormat.format(creationTime)
+        } else {
+            binding.infoDateText.text = "가입일을 불러올 수 없습니다."
+        }
     }
 }
