8afc810a321e8b3663b686f9f5c6f1e447db992f
nunuhelios <nunuhelios@sookmyung.ac.kr>
2024-11-28 10:52:35 +0900

    add permission and dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-auth-ktx:23.1.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
