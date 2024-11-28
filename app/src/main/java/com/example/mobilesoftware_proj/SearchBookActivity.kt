908872113fd890759a5f789ac795309f73d4ecef
nunuhelios <nunuhelios@sookmyung.ac.kr>
2024-11-28 11:39:18 +0900

    add book search button

--- app/src/main/java/com/example/mobilesoftware_proj/SearchBookActivity.kt
+++ app/src/main/java/com/example/mobilesoftware_proj/SearchBookActivity.kt
@@ -5,16 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mobilesoftware_proj.databinding.ActivitySearchBookBinding
 
 class SearchBookActivity : AppCompatActivity() {
+    val binding by lazy { ActivitySearchBookBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_search_book)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
     }
 }
