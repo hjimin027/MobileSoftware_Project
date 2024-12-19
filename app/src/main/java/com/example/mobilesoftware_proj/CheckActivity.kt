0642500cd2ace5cb41601f4e127207c4f5947db4
hjimin027 <hjimin027@gmail.com>
2024-12-19 18:35:44 +0900

    fix error opening CheckActivity

--- app/src/main/java/com/example/mobilesoftware_proj/CheckActivity.kt
+++ app/src/main/java/com/example/mobilesoftware_proj/CheckActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mobilesoftware_proj
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,12 +23,14 @@ import com.example.mobilesoftware_proj.databinding.ActivityCheckBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Calendar
 
 class CheckActivity : AppCompatActivity() {
     private val binding by lazy { ActivityCheckBinding.inflate(layoutInflater) }
     private val db = FirebaseFirestore.getInstance()
     private val userId = FirebaseAuth.getInstance().currentUser?.uid
 
+    @SuppressLint("DefaultLocale")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -64,6 +68,10 @@ class CheckActivity : AppCompatActivity() {
             loadBooksForDate(selectedDate)
         }
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
+
+        val today = Calendar.getInstance()
+        val currentDate = String.format("%04d.%02d.%02d", today.get(Calendar.YEAR), today.get(Calendar.MONTH) + 1, today.get(Calendar.DAY_OF_MONTH))
+        loadBooksForDate(currentDate)
     }
 
     private fun loadBooksForDate(date: String) {
@@ -128,27 +136,35 @@ class CheckActivity : AppCompatActivity() {
                     .get()
                     .addOnSuccessListener { documents ->
                         for (document in documents) {
-                            val currentPage = document.get("currentPage") as Long
+                            val currentPage = document.get("current_page") as Long
                             book.previousPage = currentPage.toInt()
                         }
                     }
             }
 
             holder.checkBox.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked){
-                    holder.todayPage.setText(book.goalPages.toString())
-                    saveCurrentPage(book.title, book.goalPages)
-                } else {
-                    holder.todayPage.setText("")
-                    saveCurrentPage(book.title, book.previousPage)
+                try {
+                    if (isChecked) {
+                        holder.todayPage.setText(book.goalPages.toString())
+                        saveCurrentPage(book.title, book.goalPages)
+                    } else {
+                        holder.todayPage.setText("")
+                        saveCurrentPage(book.title, book.previousPage)
+                    }
+                } catch (e: Exception) {
+                    Log.e("CheckActivity", e.toString())
                 }
             }
 
 
             holder.todayPage.addTextChangedListener(object : TextWatcher{
                 override fun afterTextChanged(s: Editable?) {
-                    val currentPage = s.toString().toIntOrNull() ?: 0
-                    saveCurrentPage(book.title, currentPage)
+                    try{
+                        val currentPage = s.toString().toIntOrNull() ?: 0
+                        saveCurrentPage(book.title, currentPage)
+                    } catch (e: Exception) {
+                        Log.e("CheckActivity", e.toString())
+                    }
                 }
 
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
@@ -171,7 +187,7 @@ class CheckActivity : AppCompatActivity() {
                             .document(userId)
                             .collection("user_books")
                             .document(bookId)
-                            .update("currentPage", currentPage)
+                            .update("current_page", currentPage)
                             .addOnSuccessListener {
                                 Toast.makeText(this@CheckActivity, "저장되었습니다.", Toast.LENGTH_SHORT).show()
                             }
